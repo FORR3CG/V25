94dd22db2af40f13bcb0814015236c3301bc7917
gestskoli <ges@tskoli.is>
2025-01-17 10:43:20 +0000

    unit og tuple struct

--- Timar/klasar_1/src/main.rs
+++ Timar/klasar_1/src/main.rs
@@ -1,6 +1,29 @@
 mod bill;
 use bill::Bill;
 
+// tuple struct
+#[derive(Debug)]
+struct Punktur(i32, i32);
+
+impl Punktur {
+    fn new(x: i32, y: i32) -> Self {
+        Self(x, y)
+    }
+
+    fn prenta(&self) {
+        println!("x: {}, y: {}", self.0, self.1)
+    }
+}
+
+// unit struct, á engin gögn
+struct Reikniadgerdir;
+
+impl Reikniadgerdir {
+    fn leggja_saman(a: i32, b: i32) -> i32 {
+        a + b
+    }
+}
+
 fn main() {
     let mut volvo = Bill::new(12, "Volvo");
     volvo.prenta();
