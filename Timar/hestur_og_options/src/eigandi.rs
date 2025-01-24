b85b15246c65c7042e103bfb7dfb73a906420970
gestskoli <ges@tskoli.is>
2025-01-24 10:42:51 +0000

    hestur með optional eiganda

---
+++ Timar/hestur_og_options/src/eigandi.rs
@@ -0,0 +1,22 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+pub struct Eigandi {
+    nafn: String,
+    netfang: String,
+}
+
+impl Eigandi {
+    pub fn new(nafn: &str, netfang: &str) -> Self {
+        Self {
+            nafn: nafn.to_string(),
+            netfang: netfang.to_string(),
+        }
+    }
+}
+
+impl Display for Eigandi {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "nafn: {}, email: {}", self.nafn, self.netfang)
+    }
+}
