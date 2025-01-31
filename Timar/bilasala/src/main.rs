966bd1b1ac0431f235e2a7ec0656bc601160a1ae
gestskoli <ges@tskoli.is>
2025-01-31 10:46:46 +0000

    notendaviðmót

--- Timar/bilasala/src/main.rs
+++ Timar/bilasala/src/main.rs
@@ -2,6 +2,8 @@ mod bill;
 mod gerd;
 mod bilasala;
 
+use std::io::Write;
+
 use bilasala::Bilasala;
 
 fn main() {
@@ -10,4 +12,30 @@ fn main() {
     bs.skra("Toyota", "j", 12_345_673);
     bs.skra("Nissan", "veit ekki", 650);
     println!("{}", bs)
+
+    // hætta
+    // hjálp
+    // "skrá" "bmw" "f" "200000"
+    // afskrá 1001
+    // prenta allt
+    // prenta gerd j
+    // prenta bíl 1001
+
+    loop {
+        print!("Sláðu inn skipun: ");
+        std::io::stdout().flush().expect("Gat ekki flush-að!");
+        let mut inntak = String::new();
+        std::io::stdin().read_line(&mut inntak).expect("Gat ekki lesið frá notanda!");
+        let skipanir: Vec<&str> = inntak.split_whitespace().collect(); 
+        match skipanir.first() {
+            Some(skipun) => {
+                match skipun.to_lowercase().trim() {
+                   "hætta" => break, 
+                   "hjálp" | "h" => println!("Prentum út hjálpina."),
+                   "skrá" => 
+                }
+            },
+            None => println!("Þú verður að slá eitthvað inn!"),
+        }
+    }
 }
