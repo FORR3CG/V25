eeb225a76b5ba425fb14fbc1b230b92673940677
gestskoli <ges@tskoli.is>
2025-02-13 14:22:47 +0000

    raðaður hestur

--- Timar/hestar_og_sort/src/main.rs
+++ Timar/hestar_og_sort/src/main.rs
@@ -1,11 +1,33 @@
 use std::fmt::Display;
 
-#[derive(Debug)]
+#[derive(Debug, Eq)]
 struct Hestur {
     id: u32,
     nafn: String,
     aldur: u8,
 }
+
+impl PartialEq for Hestur {
+    fn eq(&self, other: &Self) -> bool {
+        self.nafn == other.nafn && self.aldur == other.aldur
+    }
+}
+
+impl Ord for Hestur {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        if self.nafn == other.nafn {
+            self.aldur.cmp(&other.aldur)
+        } else  {
+            self.nafn.cmp(&other.nafn)
+        }
+    }
+}
+
+impl PartialOrd for Hestur {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(&other))
+    }
+} 
 
 impl Display for Hestur {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
@@ -13,10 +35,30 @@ impl Display for Hestur {
     }
 }
 
+fn prenta_hesta(hestar: &[Hestur]) {
+    hestar.iter().for_each(|hestur| println!("{}", hestur))
+}
+
 fn main() {
     let mut hestar: Vec<Hestur> = Vec::new();
     hestar.push(Hestur {id: 99, nafn: "Sæla".to_string(), aldur: 14});
     hestar.push(Hestur {id: 23, nafn: "Blési".to_string(), aldur: 9});
     hestar.push(Hestur {id: 45, nafn: "Rauður".to_string(), aldur: 11});
-    hestar.push(Hestur {id: 21, nafn: "Sæla".to_string(), aldur: 13});   
+    hestar.push(Hestur {id: 21, nafn: "Sæla".to_string(), aldur: 13});  
+
+    prenta_hesta(&hestar);
+    hestar.sort();
+    println!("----------------------------------");
+    prenta_hesta(&hestar);
+    
+    println!("----------------------------------");
+    println!("{:?}", 3.cmp(&2)); 
+    println!("{:?}", 3.cmp(&3));
+    println!("{:?}", 3.cmp(&4));
 }
+
+/* enum Ordering {
+    Less = -1,
+    Equal = 0,
+    Greater = 1,
+} */
