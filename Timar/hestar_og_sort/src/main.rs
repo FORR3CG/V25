d40c5b3dfef2456c67e54bf2d3cbc38b13ccf501
gestskoli <ges@tskoli.is>
2025-02-13 13:48:17 +0000

    raðaður hestur

--- Timar/hestar_og_sort/src/main.rs
+++ Timar/hestar_og_sort/src/main.rs
@@ -1,3 +1,18 @@
+use std::fmt::Display;
+
+#[derive(Debug)]
+struct Hestur {
+    id: u32,
+    nafn: String,
+    aldur: u8,
+}
+
+impl Display for Hestur {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "id: {}, nafn: {}, aldur: {}", self.id, self.nafn, self.aldur)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    
 }
